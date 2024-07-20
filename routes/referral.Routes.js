d1b18ce4b6556468db7a08a71ba4ae6c2c6aaa3c
agastya221 <agastyash213@gmail.com>
2024-07-20 14:29:24 +0530

    route updated

--- routes/referral.Routes.js
+++ routes/referral.Routes.js
@@ -1,9 +1,10 @@
 
 import express from 'express';
-import { createReferral } from '../controllers/referralController.js';
+import { createReferral, getAllReferrals } from '../controllers/referralController.js';
 
 export const approuter = express.Router();
 
 approuter.post('/referral', createReferral);
+approuter.get('/getreferral', getAllReferrals);
 
 
